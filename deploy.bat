f5c28aed8aafee27d2f7269aea9160453023361e
Tsuyoshi Ushio <ushio@simplearchitect.com>
2017-02-22 16:54:41 +0900

    Fix: build fail because of node_module was not included

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,9 @@
 call tsc -p .
 
 copy code.js tasks\general
+
+IF EXIST tasks\general\node_modules ( 
+RD /s /q tasks\general\node_modules 
+)
+
+xcopy /Y /I /S node_modules tasks\\general\\node_modules
